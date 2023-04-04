577a004459921892a085e400e9581f61e49b1d5e
WayanDev <wayantrk@gmail.com>
2023-04-04 09:24:53 +0700

    second commit

---
+++ admin/index.php
@@ -0,0 +1,17 @@
+<?php
+include_once 'top.php';
+include_once 'menu.php';
+?>
+
+<div>
+    <main>
+        <div class="container-fluid px-4">
+            <h1>Selamat Datang di Halaman Admin</h1>
+        </div>
+    </main>
+</div>
+
+<?php
+include_once 'bottom.php';
+?>
+
